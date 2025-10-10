e53edbd18b9bc8f23cbcf3e6e371bcb203f717a2
Chunsen Wang <chunsenwang@tubi.tv>
2025-10-10 19:07:28 +0800

    support multiple platform build

--- docker/build-and-push-image.sh
+++ docker/build-and-push-image.sh
@@ -1,16 +1,24 @@
 #! /bin/bash
 
-## set this dir as the current dir
-cd "$(dirname "$0")"
+#set -x
 
+### set this dir as the current dir
+cd "$(dirname "$0")"
 cd ..
-## build the image
+
+### build the image
 ./gradlew clean wiremock-grpc-extension-standalone:shadowJar
 
-## extract version from jar file extension
+### extract version from jar file extension
 version=$(ls wiremock-grpc-extension-standalone/build/libs/wiremock-grpc-extension-standalone-*.jar | sed 's/.*wiremock-grpc-extension-standalone-\(.*\).jar/\1/')
 
-docker build -t chunsenwang/wiremock-grpc:${version} -f docker/Dockerfile .
+manifest=docker.io/chunsen/wiremock-grpc:${version}
+podman manifest create $manifest
 
-## push the image to docker hub
-docker push chunsenwang/wiremock-grpc:${version}
+#
+podman build \
+--platform linux/arm/v7,linux/arm64,linux/amd64 \
+-f docker/Dockerfile \
+--manifest $manifest .
+
+podman manifest push $manifest
